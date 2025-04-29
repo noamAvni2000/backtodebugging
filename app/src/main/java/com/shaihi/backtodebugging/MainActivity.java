faa630f3aeafb39fe481694e707e56e12307395c
Noamavni2000 <nmnby7399@gmail.com>
2025-04-29 14:13:23 +0300

    code fixed

--- app/src/main/java/com/shaihi/backtodebugging/MainActivity.java
+++ app/src/main/java/com/shaihi/backtodebugging/MainActivity.java
@@ -1,24 +1,28 @@
 package com.shaihi.backtodebugging;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 // import android.widget.Toast; // TODO: Fix missing import
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    private string text = "Hello Debugging!";
-    private TextView textview = findViewById(R.id.my_textview);
-    private Button button = findViewById(R.id.my_button);
+    private String text = "Hello Debugging!";
+    private TextView textview;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        textview = findViewById(R.id.my_textview);
+        button = findViewById(R.id.my_button);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
